0f914eab1504c4c1184ed072fc5deb63efbf4cf2
tringcooler <tringcooler@users.noreply.github.com>
2019-08-02 04:57:49 +0800

    Update TileMaker.js

--- plugins/TileMaker.js
+++ plugins/TileMaker.js
@@ -27,6 +27,8 @@ var tile_maker = (function() {
         
     };
     
+    var posadd = (p1, p2) => [p1[0] + p2[0], p1[1] + p2[1]];
+    
     var area_pool = (function() {
         
         function area_pool() {
@@ -58,13 +60,56 @@ var tile_maker = (function() {
     var tile_unit = (function() {
         
         function tile_unit(code) {
-            this._apool = {};
-            this.parse(code);
+            this._apool = new area_pool();
+            this.setup(code);
         }
         
-        tile_unit.prototype.parse = function(code) {
+        var wall_msk2pos = {
+            0x1: [-1, 0],
+            0x3: [-1, -1],
+            0x2: [0, -1],
+            0x6: [1, -1],
+            0x4: [1, 0],
+            0xa: [1, 1],
+            0x8: [0, 1],
+            0x9: [-1, 1],
+        };
+        
+        tile_unit.prototype.set_port = function() {
+            this._apool.set([-2, 0], TYPA.port);
+            this._apool.set([0, -2], TYPA.port);
+            this._apool.set([2, 0], TYPA.port);
+            this._apool.set([0, 2], TYPA.port);
+        };
+        
+        tile_unit.prototype.set_land = function() {
+            for(var x = -1; x < 2; x++) {
+                for(var y = -1; y < 2; y++) {
+                    this._apool.set([x, y], TYPA.land);
+                }
+            }
+        };
+        
+        tile_unit.prototype.set_wall = function(cw) {
+            for(var wm in wall_msk2pos) {
+                if(wm & cw) {
+                    this._apool.set(wall_msk2pos[wm], TYPA.wall);
+                }
+            }
+        };
+        
+        tile_unit.prototype.set_cent = function(cc) {
+            if(cc & TYPC.cent) {
+                this._apool.set([0, 0], TYPA.cent);
+            }
+        };
+        
+        tile_unit.prototype.setup = function(code) {
             this._events = c2e(code);
-            
+            this.set_port();
+            this.set_land();
+            this.set_wall(code);
+            this.set_cent(code);
         };
         
         return tile_unit;
