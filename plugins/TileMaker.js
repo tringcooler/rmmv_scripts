348cd9512968dc8ad3248fa1d2ff19bd54dfb805
tringcooler <tringcooler@users.noreply.github.com>
2019-08-04 05:36:36 +0800

    Update TileMaker.js

--- plugins/TileMaker.js
+++ plugins/TileMaker.js
@@ -227,10 +227,9 @@ var tile_maker = (function() {
                 var [dcode, ddc] = dinfo;
                 var dconn = !(dcode & rwm);
                 if(sdc.is_conn(ddc)) {
-                    
-                    //TODO
-                    
-                } else if(!sconn && dconn) {
+                    continue;
+                }
+                if(!sconn && dconn) {
                     ddc.val().conn -= 1;
                     if(ddc.val().conn <= 0) {
                         valid = false;
