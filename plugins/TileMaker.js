b560076b0d7303d14babfc23f932cd31f6695798
tringcooler <tringcooler@users.noreply.github.com>
2019-08-09 01:02:45 +0800

    Update TileMaker.js

--- plugins/TileMaker.js
+++ plugins/TileMaker.js
@@ -7,19 +7,22 @@ var tile_maker = (function() {
         supp_e: 0x02,
         land: 0x04,
         wall: 0x08,
-        cent: 0x10,
-        port: 0x20,
+        port: 0x10,
+        over_p: 0x20,
         slot: 0x40,
+        cent: 0x80,
         msk_supp: 0x03,
         msk_terr: 0x0c,
-        msk_port: 0x60,
         msk_area: 0xff,
         msk_evnt: 0xff00,
-        msk_ovwr: 0xfff0,
+        msk_ovwr: 0xff80,
         msk_all: 0xffff,
     };
     
-    var a_ow = (bot, top) => ((bot | top) & TYPA.msk_ovwr) | Math.max(bot & TYPA.msk_terr, top & TYPA.msk_terr, Math.min((bot & TYPA.msk_supp) + (top & TYPA.msk_supp), TYPA.land));
+    var a_ow = (bot, top) => ((bot | top) & TYPA.msk_ovwr)
+        | Math.min((bot & TYPA.port) + (top & TYPA.port), TYPA.over_p)
+        | (((bot | top) & TYPA.msk_terr) ? 0 : ((bot | top) & TYPA.slot))
+        | Math.max(bot & TYPA.msk_terr, top & TYPA.msk_terr, Math.min((bot & TYPA.msk_supp) + (top & TYPA.msk_supp), TYPA.land));
     
     var TYPC = {
         name_wall: ['left', 'up', 'right', 'down'],
