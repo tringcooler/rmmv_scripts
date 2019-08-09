eb1930585800cec74daf97b72a2e99b7a1cfe431
tringcooler <tringcooler@users.noreply.github.com>
2019-08-10 03:49:05 +0800

    Update TileMaker.js

--- plugins/TileMaker.js
+++ plugins/TileMaker.js
@@ -42,7 +42,7 @@ var tile_maker = (function() {
         name_wall: ['left', 'up', 'right', 'down'],
         wall: [0x01, 0x02, 0x04, 0x08],
         cent: 0x80,
-        msk_terr: 0xff,
+        msk_area: 0xff,
         msk_evnt: 0x7f00,
     };
     
@@ -58,7 +58,7 @@ var tile_maker = (function() {
     };
     
     var e_c2a = (c, a) => (c & TYPC.msk_evnt) | (a & ~TYPA.msk_evnt);
-    var e2c = (e, c) => (e * (TYPC.msk_terr + 1)) | (c & ~TYPC.msk_evnt) | (e ? TYPC.cent : 0);
+    var e2c = (e, c) => (e * (TYPC.msk_area + 1)) | (c & ~TYPC.msk_evnt) | (e ? TYPC.cent : 0);
     
     var posadd = (p1, p2) => [p1[0] + p2[0], p1[1] + p2[1]];
     var possub = (p1, p2) => [p1[0] - p2[0], p1[1] - p2[1]];
