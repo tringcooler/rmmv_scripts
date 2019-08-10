30c1695f1266dc669e327469429ee19b3a20b7aa
tringcooler <tringcooler@users.noreply.github.com>
2019-08-11 04:35:14 +0800

    Update TileBoard.js

--- plugins/TileBoard.js
+++ plugins/TileBoard.js
@@ -12,6 +12,8 @@ var tile_board = (function() {
     
     var a2e = a => (a & TYPA.msk_evnt) / (TYPA.msk_area + 1);
     
+    var AREA_LAYER = 5;
+    
     function map_builder(map_info, map_strr) {
         this._map_strr = map_strr;
         this._minfo = map;
@@ -19,7 +21,7 @@ var tile_board = (function() {
     
     map_builder.prototype._init_decorator = function() {
         this._decorators = [];
-        for(var [tname, tinfo] in this._minfo.tileset.base) {
+        for(var [tname, tinfo, tlayer] in this._minfo.tileset) {
             if(tinfo instanceof Array) {
                 this._decorators.push(new tile_decorator(this._map_strr, ...tinfo));
             }
@@ -32,29 +34,60 @@ var tile_board = (function() {
         }
     };
     
-    map_builder.prototype._get_tileset_base = function(area) {
-        for(var [tname, tinfo] in this._minfo.tileset.base) {
+    var get_tiles_info = function(area, tileset) {
+        var tiles_info = [...Array(AREA_LAYER)].map(v => 0);
+        for(var [tname, tinfo, tlayer] in tileset) {
+            tlayer = tlayer ? tlayer : 0;
             var tmsk = TYPA.tname;
             if(area & tmsk) {
                 if(tinfo instanceof Array) {
-                    return tinfo[0];
+                    tiles_info[tlayer] = tinfo[0];
                 } else {
-                    return tinfo;
+                    tiles_info[tlayer] = tinfo;
                 }
             }
         }
-        return null;
+        return tiles_info;
+    };
+    
+    var rng_each = function(rng, cb) {
+        var [[left, top], [width, height]] = rng;
+        for(var x = left, x < left + width; x ++) {
+            for(var y = top, y < top + height; y ++) {
+                if(cb(x, y) === false) break;
+            }
+        }
     };
     
     map_builder.prototype.build = function(rng) {
+        rng_each(rng, (tx, ty) => {
+            var area = this._map_strr.get_tile(x, y, AREA_LAYER);
+            var tinfo = get_tiles_info(area, this._minfo.tileset);
+            for(var tlayer = 0; tlayer < tinfo.length; tlayer ++) {
+                var tval = tinfo[tlayer];
+                if(tval) {
+                    this._map_strr.set_tile(x, y, tlayer, tval);
+                }
+            }
+        });
+        this._decorate(rng);
     };
     
-    map_builder.prototype.preview = function(rng) {
+    map_builder.prototype.preview = function(prv_pool, clean = false) {
+        prv_pool.each((tx, ty, area) => {
+            var tinfo = get_tiles_info(area, this._minfo.prvset);
+            for(var tlayer = 0; tlayer < tinfo.length; tlayer ++) {
+                var tval = tinfo[tlayer];
+                if(tval) {
+                    this._map_strr.set_tile(x, y, tlayer, clean ? 0 : tval);
+                }
+            }
+        });
     };
     
     var map_pool_util = {
-        get: (pos, map) => map.get_tile(pos[0], pos[1], 5),
-        set: (pos, map, val) => map.get_tile(pos[0], pos[1], 5, val),
+        get: (pos, map) => map.get_tile(pos[0], pos[1], AREA_LAYER),
+        set: (pos, map, val) => map.set_tile(pos[0], pos[1], AREA_LAYER, val),
         each: (map, cb) => map.layer_each(5, cb),
     };
     
@@ -123,16 +156,17 @@ var g_t_board = new tile_board({
         0x11: 5, 0x12:10, 0x13:5, 0x14:2, 0x15:1,
     },
     map: {
-        tileset: {
-            base: [
-                ['cent', [4000, 48, 1]],
-                ['wall', 3000],
-                ['land', [2000, 48, 1]],
-                ['none', [1000, 48, 3]],
-            ],
-            deco: [
-                ['port', [5000, 1]],
-            ],
-        }
+        tileset: [
+            ['none', [1000, 48, 3]],
+            ['land', [2000, 48, 1]],
+            ['wall', 3000],
+            ['cent', [4000, 48, 1]],
+            ['port', [5000, 1], 1],
+        ],
+        prvset: [
+            ['land', 6000, 3],
+            ['wall', 6200, 3],
+            ['cent', 6300, 3],
+        ],
     },
 }, g_map_s);
